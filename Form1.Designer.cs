f5105b9a21bdc009f322b6200f715dac739076a9
hannibaI2023 <106945180+hannibaI2023@users.noreply.github.com>
2022-06-06 12:40:31 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace boku_no_speaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.execute = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("MS PMincho", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "インプット";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(134, 41);
+            this.Input.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(224, 20);
+            this.Input.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("MS PMincho", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ランゲージ";
+            // 
+            // execute
+            // 
+            this.execute.Font = new System.Drawing.Font("MS Mincho", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.execute.Location = new System.Drawing.Point(275, 122);
+            this.execute.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(82, 37);
+            this.execute.TabIndex = 5;
+            this.execute.Text = "魔法";
+            this.execute.UseVisualStyleBackColor = true;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("MS PMincho", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 180);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "アウトプット";
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(134, 180);
+            this.output.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(224, 68);
+            this.output.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(134, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::boku_no_speaker.Properties.Resources.senku2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(542, 310);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.execute);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "僕のスピーカー";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Management.Automation;
+
+namespace boku_no_speaker
+{
+    public partial class Form1 : Form
+    {
+        //declare for English
+        string Number = "";
+        string[] Buff = new string[] {
+            "", "thousand", "million", "billion",
+            "trillion", "quadrillion", "quintillion" };
+        string[] Small = new string[] {
+            "","one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve"};
+        string[] Prefix = new string[] {
+            "", "", "twen", "thir", "four", "fif",
+            "six", "seven", "eigh", "nine" };
+        //read for English
+        SpeechSynthesizer mouth = new SpeechSynthesizer();
+        //
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //English part
+        string writesubnum(string subnum)//read blocks of 3 digits numbers (abb)
+        {
+            string result = "";
+            int realnum = Int32.Parse(subnum);
+            //
+            if (realnum < 13)
+                return Small[realnum];
+            //Read a
+            int a = realnum / 100;
+            if (a != 0)
+            {
+                result = Small[a] + " " + "hundred";
+            }
+            //Read bb
+            int b = realnum % 100;
+            string resb = "";
+            if (b != 0)
+            {
+                if (b < 13)
+                    resb = Small[b];
+                else if (b < 20)
+                    resb = Prefix[b % 10] + "teen";
+                else //b>20
+                {
+                    if ((int)(b / 10) == 4)//stupid English
+                        resb = "forty-" + Small[b % 10];
+                    else if (b % 10 != 0)
+                        resb = Prefix[(int)(b / 10)] + "ty-" + Small[b % 10];
+                    else
+                        resb = Prefix[(int)(b / 10)] + "ty";
+                }
+                if (result != "")
+                    result = result + " and " + resb;
+                else
+                    result = resb;
+            }
+            return result;
+        }
+
+        string writeEnglish()//read whole number : English
+        {
+            if (Number == "0")
+                return "zero";
+            //
+            int level = 0;
+            string endwrite = "";
+            bool negative = false;
+            if (Number[0] == '-')
+            {
+                negative = true;
+                Number = Number.Remove(0, 1);
+            }
+            for (int i = Number.Length - 3; i >= -2; i -= 3)//blocks of 3 (abb)
+            {
+                string subnum = "";
+                if (i < 0)//in case it doesn't fit
+                    subnum = Number.Substring(0, 3 - Math.Abs(i));
+                else
+                    subnum = Number.Substring(i, 3);
+                string subwrite = writesubnum(subnum);
+                if (subwrite != "")
+                    endwrite = subwrite + " " + Buff[level] + " " + endwrite;
+                //in case it fits
+                if (i == 0)
+                    return endwrite;
+                //
+                ++level;
+            }
+            if (negative)
+                endwrite = "Negative " + endwrite;
+            else endwrite = char.ToUpper(endwrite[0]) + endwrite.Substring(1);
+
+            return endwrite;
+        }
+        //end of English part
+
+        //Vietnamese part 
+        public static string NumberToText(string inputNumber, bool suffix = true)
+        {
+            string[] unitNumbers = new string[] { "không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+            string[] placeValues = new string[] { "", "nghìn", "triệu", "tỷ" };
+            bool isNegative = false;
+
+            string sNumber = inputNumber;
+            double number = Convert.ToDouble(inputNumber);
+            if (number < 0)
+            {
+                number = -number;
+                sNumber = number.ToString();
+                isNegative = true;
+            }
+
+
+            int ones, tens, hundreds;
+
+            int positionDigit = sNumber.Length;   // last -> first
+
+            string result = " ";
+
+
+            if (positionDigit == 0)
+                result = unitNumbers[0] + result;
+            else
+            {
+                int placeValue = 0;
+
+                while (positionDigit > 0)
+                {
+                    // Check last 3 digits remain ### (hundreds tens ones)
+                    tens = hundreds = -1;
+                    ones = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
+                    positionDigit--;
+                    if (positionDigit > 0)
+                    {
+                        tens = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
+                        positionDigit--;
+                        if (positionDigit > 0)
+                        {
+                            hundreds = Convert.ToInt32(sNumber.Substring(positionDigit - 1, 1));
+                            positionDigit--;
+                        }
+                    }
+
+                    if ((ones > 0) || (tens > 0) || (hundreds > 0) || (placeValue == 3))
+                        result = placeValues[placeValue] + result;
+
+                    placeValue++;
+                    if (placeValue > 3) placeValue = 1;
+
+                    if ((ones == 1) && (tens > 1))
+                        result = "mốt " + result;
+                    else
+                    {
+                        if ((ones == 5) && (tens > 0))
+                            result = "lăm " + result;
+                        else if (ones > 0)
+                            result = unitNumbers[ones] + " " + result;
+                    }
+                    if (tens < 0)
+                        break;
+                    else
+                    {
+                        if ((tens == 0) && (ones > 0)) result = "linh " + result;
+                        if (tens == 1) result = "mười " + result;
+                        if (tens > 1) result = unitNumbers[tens] + " mươi " + result;
+                    }
+                    if (hundreds < 0) break;
+                    else
+                    {
+                        if ((hundreds > 0) || (tens > 0) || (ones > 0))
+                            result = unitNumbers[hundreds] + " trăm " + result;
+                    }
+                    result = " " + result;
+                }
+            }
+            result = result.Trim();
+            if (isNegative) result = "Âm " + result;
+            return result;
+        }
+
+        //driver code
+        private void execute_Click(object sender, EventArgs e)
+        {
+            Number = Input.Text;
+            if (comboBox1.Text == "Tiếng Việt")
+            {
+                try
+                {
+                    output.Text = boku_no_speaker.Form1.NumberToText(Number);
+                    try
+                    {
+                        mouth.SelectVoice("Microsoft An");
+                        mouth.SpeakAsync(output.Text);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Bạn chưa cài đặt Language Pack Tiếng Việt!", "XẢY RA LỖI");
+                    }
+                }
+                catch//If number too big
+                {
+                    MessageBox.Show("Đã có lỗi xảy ra!", "XẢY RA LỖI", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else //if (English.Checked == true)
+            {
+                try
+                {
+                    output.Text = writeEnglish();
+                    mouth.SelectVoice("Microsoft David");
+                    mouth.SpeakAsync(output.Text);
+                }
+                catch//If number too big
+                {
+                    MessageBox.Show("Đã có lỗi xảy ra!", "XẢY RA LỖI", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hãy cài đặt Microsoft Language Pack Tiếng Việt để đọc Tiếng Việt.","CẢNH BÁO");
+            //add stuff to combo box
+            comboBox1.Items.Add("Tiếng Việt");
+            comboBox1.Items.Add("English");
+            //"unlock" VN pack
+            string scriptinstallvi = @"
+            $sourcePath = 'HKLM:\software\Microsoft\Speech_OneCore\Voices\Tokens' #Where the OneCore voices live
+            $destinationPath = 'HKLM:\SOFTWARE\Microsoft\Speech\Voices\Tokens' #For 64-bit apps
+            $destinationPath2 = 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\SPEECH\Voices\Tokens' #For 32-bit apps
+            cd $destinationPath
+            $listVoices = Get-ChildItem $sourcePath
+            foreach($voice in $listVoices)
+            {
+                $source = $voice.PSPath #Get the path of this voices key
+                copy -Path $source -Destination $destinationPath -Recurse
+                copy -Path $source -Destination $destinationPath2 -Recurse
+            }
+            ";
+            PowerShell ps1 = PowerShell.Create();
+            ps1.AddScript(scriptinstallvi);
+            ps1.Invoke();
+        }
+        //end of driver code
+    }
+}
